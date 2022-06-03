45a799d608dab1ff7e7c1f2e70b9551c3cbc6d4c
kamela williamson <kami63116@yahoo.com>
2022-06-03 16:44:43 -0700

    first commit

---
+++ script.js
@@ -0,0 +1,53 @@
+const color_box = document.getElementById("color_box");
+
+const green_button = document.getElementById("green_button");
+green_button.onclick = function () {
+  color_box.className = "green";
+};
+
+const blue_button = document.getElementById("blue_button");
+blue_button.onclick = function () {
+  color_box.className = "blue";
+};
+
+const orange_button = document.getElementById("orange_button");
+orange_button.onclick = function () {
+  color_box.className = "orange";
+};
+
+const yellow_button = document.getElementById("yellow_button");
+yellow_button.onclick = function () {
+  color_box.className = "yellow";
+};
+
+const red_button = document.getElementById("red_button");
+red_button.onclick = function () {
+  color_box.className = "red";
+};
+
+const pink_button = document.getElementById("pink_button");
+pink_button.onclick = function () {
+  color_box.className = "pink";
+};
+
+const purple_button = document.getElementById("purple_button");
+purple_button.onclick = function () {
+  color_box.className = "purple";
+};
+
+const mauve_button = document.getElementById("mauve_button");
+mauve_button.onclick = function () {
+  color_box.className = "mauve";
+};
+
+const gray_button = document.getElementById("gray_button");
+gray_button.onclick = function () {
+  color_box.className = "gray";
+};
+
+const brown_button = document.getElementById("brown_button");
+brown_button.onclick = function () {
+  color_box.className = "brown";
+};
+
+
